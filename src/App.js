035c7e4491816cceb8ccf9cc9f3fbfcb924fd87c
Paul Tyng <ptyng@underarmour.com>
2017-04-14 16:49:06 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import fuzzy from 'fuzzy';
+import Form from "react-jsonschema-form";
 
 import './App.css';
 
@@ -18,8 +19,8 @@ class HighlightText extends Component {
     return (
       <span>
         {first}
-        {highlights.map(s => s.split('<<', 2)).map(s =>
-          <span key={s}>
+        {highlights.map(s => s.split('<<', 2)).map((s, i) =>
+          <span key={i}>
             <span style={{color: 'red'}}>{s[0]}</span>
             {s[1]}
           </span>
@@ -30,44 +31,46 @@ class HighlightText extends Component {
 }
 
 class TaskPreview extends Component {
+  handleSelect(e) {
+    if (this.props.onSelect) {
+      this.props.onSelect(this.props.task);
+    }
+  }
+
   render() {
     return (
-      <li className="task-preview">
-        <h3><HighlightText text={this.props.task.title} /></h3>
-        <p><HighlightText text={this.props.task.description} /></p>
+      <li className="task-preview" onClick={this.handleSelect.bind(this)}>
+        <h3><HighlightText text={this.props.task.highlightTitle} /></h3>
+        <p><HighlightText text={this.props.task.highlightDescription} /></p>
       </li>
     )
   }
 }
 
 class TaskList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { search: '' };
-
-    this.handleSearchChange = this.handleSearchChange.bind(this);
-  }
-
   handleSearchChange(e) {
     this.setState({ search: e.target.value });
   }
 
   render() {
+    const search = this.state && this.state.search ? this.state.search : '';
     const results = fuzzy
-      .filter(this.state.search, this.props.tasks, fuzzyOptions)
+      .filter(search, this.props.tasks, fuzzyOptions)
       .map(r => {
         const strings = r.string.split(separator);
         return Object.assign({}, r.original, {
-          title: strings[0],
-          description: strings[1],
+          highlightTitle: strings[0],
+          highlightDescription: strings[1],
         });
       });
     return (
       <div className="task-list">
-        <input type="search" value={this.state.search} onChange={this.handleSearchChange} />
+        <div className="header">
+          <input placeholder="Search tasks" type="search" value={search} onChange={this.handleSearchChange.bind(this)} />
+        </div>
         <ol>
           {results.map(task =>
-            <TaskPreview key={task.name} task={task} />,
+            <TaskPreview key={task.name} task={task} onSelect={this.props.onSelect} />,
           )}
         </ol>
       </div>
@@ -75,11 +78,45 @@ class TaskList extends Component {
   }
 }
 
-class App extends Component {
+class TaskDetail extends Component {
+  handleExit() {
+    this.props.onExit && this.props.onExit();
+  }
+
   render() {
+    const schema = Object.assign({
+    }, this.props.task.schema, {
+      type: 'object',
+      title: null,
+    });
+    console.log(schema);
+    return (
+      <div className="task-detail">
+        <div className="header">
+          <h1>{this.props.task.title}</h1>
+        </div>
+        <p>{this.props.task.description}</p>
+        <Form schema={schema} />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  handleSelect(task) {
+    this.setState({ currentTask: task });
+  }
+
+  render() {
+    let body = <TaskList tasks={this.props.tasks} onSelect={this.handleSelect.bind(this)} />;
+
+    if (this.state != null && this.state.currentTask != null) {
+      body = <TaskDetail task={this.state.currentTask} onExit={() => this.handleSelect(null)} />;
+    }
+
     return (
       <div className="app">
-        <TaskList tasks={this.props.tasks} />
+        {body}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import 'normalize.css/normalize.css';
 import './index.css';
 
 const tasks = [
